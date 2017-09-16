3a7a90fd1b47fb5a9de421aa272db0ee3bcc433d
ashanaakh <ashanaakh@gmail.com>
2017-09-16 13:45:56 +0300

    improve server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
+var glob = require('glob');
+var path = require('path');
 var express = require('express');
+
 var app = express();
 
 app.use("/assets", express.static(__dirname + '/assets'));
 app.use("/node_modules", express.static(__dirname + '/node_modules'));
 
-app.use('/arlington', express.static(__dirname + '/build/arlington-review-rev'));
-app.use('/bricky', express.static(__dirname + '/build/bricky'));
-app.use('/burns', express.static(__dirname + '/build/burns'));
-app.use('/caledoniancrow', express.static(__dirname + '/build/caledoniancrow'));
-app.use('/critics', express.static(__dirname + '/build/critics'));
-app.use('/baltimore', express.static(__dirname + '/build/baltimore'));
-app.use('/beautyblog', express.static(__dirname + '/build/beautyblog'));
-app.use('/intoafrica', express.static(__dirname + '/build/intoafrica'));
-app.use('/mountaineering', express.static(__dirname + '/build/mountaineering'));
-app.use('/snailstew', express.static(__dirname + '/build/snailstew'));
+glob.sync('./build/*/*.html').forEach( function(file) {
 
-var port = process.env.PORT || 5000;
+  let folder = path.dirname(file);
+  let base = '/' + path.basename(folder);
+  let pathToFile = path.join(__dirname, folder);
+
+  app.use(base, express.static(pathToFile));
+});
+
+var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log("Listening on " + port);
