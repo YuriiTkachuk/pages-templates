79f0228dc3eafdfd2c2fb13cb3457bf6b6769777
ashanaakh <ashanaakh@gmail.com>
2017-09-20 22:41:58 +0300

    code style fixes index.js & server.js

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-'use strict'
-
-var Metalsmith  = require('metalsmith')
-var layouts     = require('metalsmith-layouts')
-var markdown    = require('metalsmith-markdown')
-var helpers = require('handlebars-helpers')
-var math = helpers.math()
+var Metalsmith  = require("metalsmith");
+var layouts     = require("metalsmith-layouts");
+var markdown    = require("metalsmith-markdown");
+var helpers     = require("handlebars-helpers");
+var math        = helpers.math();
 
 var dir = {
-  base:   __dirname + '/',
-  source: './src/',
-  dest:   './build/'
-}
+  base:   __dirname + "/",
+  source: "./src/",
+  dest:   "./build/"
+};
 
 var ms = Metalsmith(dir.base)
   .metadata({
     title: "Generator",
     description: "Sites generator with 8 themes",
-    generator: "Metalsmith",
-    url: "http://www.metalsmith.io/"
+    generator: "Metalsmith"
   })
   .source(dir.source)
   .destination(dir.dest)
   .clean(true)
   .use(markdown())
   .use(layouts({
-    engine: 'handlebars'
+    engine: "handlebars"
   }))
   .build(function(err, files) {
-    if (err) { throw err  }
-  })
+    if (err) {
+      throw err;
+    }
+  });
--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-var glob = require('glob');
-var path = require('path');
-var express = require('express');
+var glob    = require("glob");
+var path    = require("path");
+var express = require("express");
+var app     = express();
+var port    = process.env.PORT || 3000;
 
-var app = express();
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
 
-app.use("/assets", express.static(__dirname + '/assets'));
-app.use("/node_modules", express.static(__dirname + '/node_modules'));
-
-glob.sync('./build/*/*.html').forEach(function(file) {
+glob.sync("./build/*/*.html").forEach(function(file) {
   let folder = path.dirname(file);
   let name = path.basename(file);
-  let base = '/page/'  + path.basename(file, '.html');
+  let base = "/page/"  + path.basename(file, ".html");
   let pathToFile = path.join(__dirname, folder, name);
 
   app.use(base, express.static(pathToFile));
 });
 
-var port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port);
